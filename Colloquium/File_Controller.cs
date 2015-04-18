5114d513a71169ee495c8cc533e6a4d4560b4284
Mynamesparta <bot-1@i.ua>
2015-04-18 17:44:31 +0300

    begin.

---
+++ Colloquium/File_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Colloquium
+{
+    public class File
+    {
+        string file_name;// = "result.txt";
+        //string path=
+        FileInfo file;
+
+        public File(string name)
+        {
+            file_name = name;
+            file = new FileInfo(System.IO.Directory.GetCurrentDirectory() + name);
+            Console.WriteLine(file.FullName);
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
+        public void Write(string text)
+        {
+            using (StreamWriter sw = file.AppendText())
+            {
+                sw.WriteLine(text);
+            }
+        }
+        public string Read()
+        {
+            string text;
+            using (StreamReader sw = file.OpenText())
+            {
+                text=sw.ReadToEnd();
+            }
+            return text;
+        }
+    }
+}
---
+++ Colloquium/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Colloquium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File result_file = new File("_result.txt");
+            result_file.Write("ok");
+            Console.Read();
+        }
+    }
+}
