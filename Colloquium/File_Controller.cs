75c00decef1c0ce9fbf4e3a12459270831f65c20
Mynamesparta <bot-1@i.ua>
2015-04-18 19:12:25 +0300

    1.0

--- Colloquium/File_Controller.cs
+++ Colloquium/File_Controller.cs
@@ -15,11 +15,18 @@ namespace Colloquium
         public File(string name)
         {
             file_name = name;
-            file = new FileInfo(System.IO.Directory.GetCurrentDirectory() + name);
-            Console.WriteLine(file.FullName);
+            
+            //
+            file_name = System.IO.Directory.GetCurrentDirectory() +'\\'+ name;
+            //file_name.Replace("Debug", "");
+            file = new FileInfo(file_name);
+            Console.WriteLine(file_name);
             if (file.Exists)
             {
-                file.Delete();
+                Console.WriteLine("nice");
+            }
+            else
+            {
             }
         }
         public void Write(string text)
@@ -32,11 +39,49 @@ namespace Colloquium
         public string Read()
         {
             string text;
-            using (StreamReader sw = file.OpenText())
+            Console.WriteLine(file.FullName);
+            if (file.Exists)
+            {
+                Console.WriteLine("nice world");
+            }
+            StreamReader sw = file.OpenText();
             {
                 text=sw.ReadToEnd();
             }
             return text;
+        }
+        public void readPoint(ref List<Point> list)
+        {
+            Point point;
+            string[] number;
+            string text;
+            int lenght=new int();
+            float a = new float();
+            using (StreamReader sw = file.OpenText())
+            {
+                int.TryParse(sw.ReadLine(), out lenght);
+                Console.WriteLine("Lenght:" + lenght);
+                text = sw.ReadLine();
+                while (text!=""&&text!=" ")
+                {
+                    Console.WriteLine(text);
+                    if (sw.EndOfStream)
+                        break;
+                    number = text.Split(' ');
+                    point = new Point();
+                    float.TryParse(number[0], out a);
+                    point.x = float.Parse(number[0].ToString());
+                    point.x = a;
+                    float.TryParse(number[1], out a);
+                    point.y = a;
+                    float.TryParse(number[2], out a);
+                    point.z = a;
+                    list.Add(point);
+                    text = sw.ReadLine();
+                    Console.WriteLine("point:"+point.x + " " + point.y + " " + point.z);
+                }
+            }
+            //return list;
         }
     }
 }
--- Colloquium/Program.cs
+++ Colloquium/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 namespace Colloquium
 {
+    public struct Point
+    {
+        public float x, y, z;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            File result_file = new File("_result.txt");
-            result_file.Write("ok");
+            File input_file = new File("input.txt");
+            //input_file.Write("hello");
+            //Console.Write(input_file.Read());
+            List<Point> list_of_point = new List<Point>();
+            input_file.readPoint(ref list_of_point);
+            Console.WriteLine("===");
+            foreach (Point point in list_of_point)
+            {
+                Console.WriteLine(point.x + " " + point.y + " " + point.z);
+            }
+            //
             Console.Read();
         }
     }
